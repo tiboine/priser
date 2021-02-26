c98080d71493c112fcfaac14b26966863fe320c1
tiboine <urbanthor@gmail.com>
2021-02-26 09:04:20 +0100

    Squashed a bug!

--- generate_pricelist.py
+++ generate_pricelist.py
@@ -7,10 +7,10 @@ product = gs.sheets
 chromaluxe = product[1][5]  # chromaluxe description
 lerret = product[1][6]  # Canvas description
 storformat = product[1][7]
-
 priceChroma_2_3 = {}
 priceLerret_2_3 = {}
 priceStorformat_2_3 = {}
+
 
 priceList = ['Name', 'Type', 'Size', 'Price',
              'Description', 'Imageloc', 'category', 'tags', 'SKU', 'variationDesc']
@@ -19,7 +19,6 @@ with open('pricelist2.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(priceList)  # headers
     for x in range(1, len(product)):
-
         writer.writerow([product[x][0], '', '', '',
                          product[x][1],
                          product[x][2],
@@ -27,13 +26,13 @@ with open('pricelist2.csv', 'w', newline='', encoding='utf-8') as file:
                          product[x][4],
                          product[x][8],
                          ])
-        if product[x][9] == '2:3' or product[x][9] == '3:2':
+        if product[x][9] == '2:3' or '3:2':
             if product[x][9] == '2:3':
                 for i, val in gs.priceChroma.items():
                     i = str(i).split('x')
                     priceChroma_2_3[f"{i[1]}x{i[0]}"] = val
 
-                prices = priceChroma_2_3.items()
+                    prices = priceChroma_2_3.items()
             elif product[x][9] == '3:2':
                 prices = gs.priceChroma.items()
 
@@ -43,10 +42,10 @@ with open('pricelist2.csv', 'w', newline='', encoding='utf-8') as file:
                      product[0][5],
                      size,
                      price, '', '', '', '',
-                     product[x][7],
+                     '',
                      chromaluxe])
 
-            if product[x][9] == '2:3':
+            if product[x][9] == '2:3' or '3:2':
                 for i, val in gs.priceLerret.items():
                     i = str(i).split('x')
                     priceLerret_2_3[f"{i[1]}x{i[0]}"] = val
@@ -59,11 +58,10 @@ with open('pricelist2.csv', 'w', newline='', encoding='utf-8') as file:
                     [product[x][0],
                      product[0][6],
                      size,
-                     price, '', '', '',  '',
-                     product[x][7],
+                     price, '', '', '',  '', '',
                      lerret])
 
-            if product[x][9] == '2:3':
+            if product[x][9] == '2:3' or '3:2':
                 for i, val in gs.priceStorformat.items():
                     i = str(i).split('x')
                     priceStorformat_2_3[f"{i[1]}x{i[0]}"] = val
@@ -76,12 +74,12 @@ with open('pricelist2.csv', 'w', newline='', encoding='utf-8') as file:
                     [product[x][0],
                      product[0][7],
                      size,
-                     price, '', '', '',  '',
-                     product[x][7],
+                     price, '', '', '',  '',  '',
                      storformat])
 
-        if product[x][9] == '2:1':
+        elif product[x][9] == '2:1':
             prices = gs.priceStorformatPano.items()
+            print(prices)
 
             for size, price in prices:
                 writer.writerow(
@@ -91,3 +89,5 @@ with open('pricelist2.csv', 'w', newline='', encoding='utf-8') as file:
                      price, '', '', '',  '',
                      product[x][7],
                      storformat])
+
+print(prices)
