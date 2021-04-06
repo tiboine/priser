9280fae5167a364d6223a69ef67f2c30f8cc4e3c
tiboine <urbanthor@gmail.com>
2021-04-06 21:00:08 +0200

    cut some fat

--- generate_pricelist.py
+++ generate_pricelist.py
@@ -1,6 +1,5 @@
 import importGoogleSheets as gs
 import csv
-import collections
 gs.gsheets()
 product = gs.sheets
 productSizeVertical = []
@@ -10,69 +9,61 @@ productVarVertical = []
 varDesc = {}
 
 
-class Products:
-    pass
-
-
-myList = []
-# for i in range(len(gs.productVar)):
-myList.append(gs.productVar)
-
-# print(myList)
-
 # konvertere 3:2 størrelser til 2:3
-for i in range(len(gs.productSize)):
+for num in range(len(gs.productSize)):
     temp = []
-    for j in gs.productSize[i]:
-        y = str(j).split('x')
-        temp.append(f'{y[1]}x{y[0]}')
+    for size in gs.productSize[num]:
+        splitSize = str(size).split('x')
+        temp.append(f'{splitSize[1]}x{splitSize[0]}')
     productSizeVerticalTemp.append(temp)
 
 # lage nye lister med kun 2:3 produkter
 kortProductVar_2_3 = []
-for i in range(len(gs.productVar)):
-    if '3:2' in gs.productVar[i]:
-        productSizeVertical.append(productSizeVerticalTemp[i])
-        productPriceVertical.append(gs.productPrice[i])
-        productVarVertical.append(gs.productVar[i])
+for num in range(len(gs.productVar)):
+    if '3:2' in gs.productVar[num]:
+        productSizeVertical.append(productSizeVerticalTemp[num])
+        productPriceVertical.append(gs.productPrice[num])
+        productVarVertical.append(gs.productVar[num])
         kortProductVar_2_3.append([x.strip()
-                                   for x in gs.productVar[i].split(' ')][0])  # strippe 2:3 fra typen
+                                   for x in gs.productVar[num].split(' ')][0])  # strippe 2:3 fra typen
 
-
-# LAGE egen productVar strip
+# Fjerne ratiotall fra productVar. fineart 3:2 -> fineart
 kortProductVar = []
-for x in range(len(gs.productVar)):
-    strippedProductVar = [x.strip() for x in gs.productVar[x].split(' ')]
+for num in range(len(gs.productVar)):
+    strippedProductVar = [num.strip() for num in gs.productVar[num].split(' ')]
     strippedProductVar.pop(1)
     kortProductVar.append(strippedProductVar[0])
 
 # lager dict for lettere plassering av variationDesc
-for i in range(len(gs.productVar)):
+for num in range(len(gs.productVar)):
     for y in range(1, 4):
-        if product[0][11 + y].lower() in gs.productVar[i].lower():
-            varDesc[gs.productVar[i]] = product[1][11+y]
+        if product[0][11 + y].lower() in gs.productVar[num].lower():
+            varDesc[gs.productVar[num]] = product[1][11+y]
 
 
-with open('Landskapspriser.csv', 'w', newline='', encoding='utf-8') as file:
+with open(str(gs.RANGE_NAME[:-7])+'.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(product[0])  # headers
 
-    for x in range(1, len(product)):
+    class Products:
+        pass
+
+    for num in range(1, len(product)):
         image = Products()
-        image.name = product[x][0]
-        image.description = product[x][4]
-        image.imageloc = product[x][5]
-        image.category = product[x][6]
-        image.tags = product[x][7]
-        image.sku = product[x][8]
+        image.name = product[num][0]
+        image.description = product[num][4]
+        image.imageloc = product[num][5]
+        image.category = product[num][6]
+        image.tags = product[num][7]
+        image.sku = product[num][8]
         image.variationdesc = product[1][13]
 
         # writer.writerow(
         #     [image.name, '', '', '', image.description, image.imageloc, '', '', image.sku, '', ''])
 
         for i in range(len(gs.productVar)):
-            if product[x][11] in gs.productVar[i]:
-                for j in range(len(gs.productPrice[i])):
+            for j in range(len(gs.productPrice[i])):
+                if '3:2' in gs.productVar[i]:
                     writer.writerow(
                         [image.name,
                          str(kortProductVar[i]).capitalize(),
@@ -90,7 +81,7 @@ with open('Landskapspriser.csv', 'w', newline='', encoding='utf-8') as file:
 
         for i in range(len(productVarVertical)):
             for j in range(len(productPriceVertical[i])):
-                if '2:3' in product[x][11]:
+                if '2:3' in product[num][11]:
                     writer.writerow(
                         [image.name,
                          str(kortProductVar_2_3[i]).capitalize(),
@@ -105,5 +96,4 @@ with open('Landskapspriser.csv', 'w', newline='', encoding='utf-8') as file:
                          image.sku,
                          list(varDesc.values())[i + 1]])
 
-print('Antall produkter:')
-print(len(product))
+print('Antall produkter:', len(product))
